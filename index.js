a019a7e290fa418e4e0ad44b323da2eb5247de53
Yohann Andre <y.andre90000@gmail.com>
2023-11-15 18:07:53 +0100

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const cors = require("cors")
 const app = express()
 app.use(express.json())
+
+app.use(cors)
 
 // Routes
 const charactersRoute = require('./routes/characters')
